ecbc39d04088bcd32d929f72851fa18a09a0b48a
Cip <xeonsat@gmail.com>
2021-06-22 16:01:47 +0800

    myPWA

---
+++ myProgressiveWebApp/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch('https://orangevalleycaa.org/api/videos').then(
+        response => response.json()
+      )
+      setData(result);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      <header>
+        <h1>My Progressive Web App</h1>
+      </header>
+      {data.map(video => (
+        <div key={video.id}>
+          <h2>{video.name}</h2>
+          <video height={200} controls src={video.video_url} />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
